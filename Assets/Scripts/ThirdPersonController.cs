7cbc173ea5eaa2f6c6406b9cd9f87c7c3fc88a59
rabeekiblawi <rabeekiblawi@gmail.com>
2021-10-15 22:04:53 +0300

    created 3rd person conrtroller

---
+++ Assets/Scripts/ThirdPersonController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Script Ref https://www.youtube.com/watch?v=4HpC--2iowE
+public class ThirdPersonController : MonoBehaviour
+{
+    public CharacterController controller;
+    public Transform cam;
+    public float speed = 6f;
+    public float turnSmoothTime = 0.1f;
+    float turnSmoothVelocity;
+
+    private void Update()
+    {
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        float verticalInput = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizontalInput, 0, verticalInput).normalized;
+
+        if (direction.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0, angle, 0);
+            Vector3 moveDir = Quaternion.Euler(0, targetAngle, 0) * Vector3.forward;
+            controller.Move(transform.forward * speed * Time.deltaTime);
+        }
+    }
+}
